6016f615a6cde5704e5f1d08e3e259f53f91954e
Tim Hadley <hadleyt@gmail.com>
2025-09-07 18:19:57 -0600

    try to add orgmode plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,15 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+
+-- TPH Orgmode:
+return {
+  'nvim-orgmode/orgmode',
+  event = 'VeryLazy',
+  config = function()
+    require('orgmode').setup {
+      org_agenda_files = '~/orgfiles/**/*',
+      org_default_notes_file = '~/orgfiles/refile.org',
+    }
+  end,
+}
